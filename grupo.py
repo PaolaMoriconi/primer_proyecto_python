e6e25b45ef6b6fde141e4b7330a9e6492591464a
Noeli Paola Moriconi <paomori18@gmail.com>
2025-01-26 22:19:19 -0300

    Agrego la funcion para capturar y validar el caracter

--- grupo.py
+++ grupo.py
@@ -1,4 +1,9 @@
+##Proyecto: Juego de ahorcado
+##Integrantes:Noeli Paola Moriconi y Gisele Lorena Ortiz
+##Importo random para seleecionar de forma aleatoria una indice del array con las palabras del archivo
 import random
+
+##importo re para validar que la letra ingresada sea una letra y no un numero o caracter especial
 import re
 
 # Leer un archivo de texto y transformarlo en un array
@@ -23,3 +28,37 @@ def set_parametros(dificultad):
         return {"vidas": 5, "ruta_archivo": './palabras_medio.txt'}
     elif dificultad == "dificil":
         return {"vidas": 7, "ruta_archivo": './palabras_dificil.txt'}
+    
+# Capturar la letra ingresando por el usuario
+def capturar_letra():
+    letra = input("Ingresa una letra: ")
+    while not re.match("^[a-zA-Z]*$", letra) or len(letra) != 1:
+        print("Por favor, ingresa una letra valida")
+        letra = input("Ingresa una letra: ")
+    return letra
+
+#Seteo parametros
+dificultad = seleccionar_dificultad()
+parametros = set_parametros(dificultad)
+vidas = parametros["vidas"]
+ruta_archivo = parametros["ruta_archivo"]
+
+# Leer el archivo y transformarlo en un array
+array_lineas = leer_archivo_como_array(ruta_archivo)
+
+#capturo la longitud del array
+longitud = len(array_lineas)
+
+# Asignar un número aleatorio
+numero_aleatorio = random.randint(0, longitud - 1)
+#selecciono una palabra aleatoria del array
+palabra_selecionada = array_lineas[numero_aleatorio]
+
+#Inicializo variable que indica si el juego termino
+end_game = False
+caracteres_incorrectos = []
+respuesta = ['_'] * len(palabra_selecionada)
+
+print(f"La palabra seleccionada contiene {len(palabra_selecionada)} caracteres")
+print("\n")
+
